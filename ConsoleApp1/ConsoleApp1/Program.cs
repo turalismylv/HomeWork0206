e1cb79640cdaaa985aeebef860287cd09c1f51b3
turalismylv <turalhi@code.edu.az>
2022-06-02 12:41:58 +0400

    HomeWork0206

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+
+
+        {
+            //            Tapşırıq:
+
+            //            Asagidaki metodlari olan IAccount interface yaradirsiniz : PasswordChecker() - parameter oalraq string sifreni qebul edir
+            //            ShowInfo()
+            //            Asagidaki memberlerden ibaret User class-i yaradirsiniz ve IAccount-u implement edirsiniz:
+            //Fullname
+            //Email
+            //Password
+            //PasswordChecker() - PasswordChecker metodu - gelen string sifre deyerinin sertleri odeyib odemediyini yoxlayib true/false deyer qaytarir.Şertler: - sifrede minimum 8 character olmalidir -
+            //ifrede en az 1 boyuk herf olmaldir - sifrede en az 1 kicik herf olmalidir - sifrede en az 1 reqem olmalidir
+            //ShowInfo() - bu metod Console-a user-in Fullanem ve email-inin yazdirir
+            //ps: User classina IAccount interface-i implement edilir.User yarandigi zaman email ve sifre teyin edilmesi mecburidir.
+            //Usere sifre teyin edilerken sifrenin PasswordChecker metodunun sertlerini odemesi lazimdir.
+            User us1 = new User();
+
+            us1.Email = "turalhi@code.edu.az";
+            us1.FullName = "Tural Ismayilov";
+            us1.Password = "Tural123";
+
+            
+            us1.ShowInfo();
+            Console.WriteLine(us1.PasswordChecker());
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/User.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public interface IAccount
+    {
+
+        public abstract bool PasswordChecker();
+            
+           
+        
+        public abstract void ShowInfo();
+        
+            
+        
+        
+
+
+    }
+    class User : IAccount
+    {
+        public string FullName { get; set; }
+        public  string Email { get ; set ; }
+        public  string Password { get ; set ; }
+
+        public  bool PasswordChecker()
+        {
+            int LowerCount = 0;
+            int UpperCount = 0;
+            int DigitCount = 0;
+            for (int i = 0; i < Password.Length; i++)
+            {
+                if (Password.Length > 7)
+                {
+                    if (Char.IsLower(Password[i]))
+                    {
+                        LowerCount++;
+                    }
+                    else if (Char.IsUpper(Password[i]))
+                    {
+                        UpperCount++;
+                    }
+                    else if (Char.IsDigit(Password[i]))
+                    {
+                        DigitCount++;
+                    }
+                    if (LowerCount > 0 && UpperCount > 0 && DigitCount > 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+
+        public  void ShowInfo()
+        {
+            Console.WriteLine($"User fullname:{FullName} " +
+                $"User email:{Email}");
+        }
+
+
+
+    }
+}
